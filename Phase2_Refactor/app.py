6dc057ca00f88376881cba20922bc417d2b21e33
Chris <cnevins@udel.edu>
2026-05-12 23:12:27 -0400

    Sidebar Completed

--- Phase2_Refactor/app.py
+++ Phase2_Refactor/app.py
@@ -17,6 +17,9 @@ if "role" not in st.session_state:
 if "page" not in st.session_state:
     st.session_state["page"] = "login"
 
+if "employee_dashboard_view" not in st.session_state:
+    st.session_state["employee_dashboard_view"] = "tabs"
+
 #JSON
 if "inventory" not in st.session_state:
     st.session_state["inventory"] = data_manager.load_data(Path("Phase2_Refactor/inventory.json"))
@@ -32,6 +35,14 @@ if "AI" not in st.session_state:
 
 from ui import auth_ui
 
+def log_out():
+    st.session_state["logged_in"] = False
+    st.session_state["user"] = None
+    st.session_state["role"] = None
+    st.session_state["page"] = "login"
+    st.session_state["employee_dashboard_view"] = "tabs"
+    st.rerun()
+
 if not st.session_state["logged_in"]:
     auth_ui.log_in()
     auth_ui.reg_render("Phase2_Refactor/users.json")
@@ -40,73 +51,107 @@ if not st.session_state["logged_in"]:
 elif st.session_state["role"] == "Owner":
     if st.session_state["page"] == "home":
         st.markdown("Welcome! This is the Owner dashboard")
-        if st.button("Go to Dashboard", key= "dashboard_view_btn", type= "primary", use_container_width=True):
-            st.session_state["page"] = "dashboard"
-            st.rerun()
+
     elif st.session_state["page"] == "dashboard":
-        st.markdown("Dashboard")    
 
-        if st.button("Log out", type="primary", width='content'):
-            st.session_state["logged_in"] = False
-            st.session_state["user"] = None
-            st.session_state["role"] = None
-            st.session_state["page"] = "login"
-            import time
-            time.sleep(4)
-            st.rerun()
-
-        tab1, tab2, tab3, tab4, tab5 = st.tabs(["Add New Product", "Update Prices", "Restocking", "Deleting", "AI Chatbot"])    
-
-        with tab1:
+        if st.session_state["employee_dashboard_view"] == "adding product":
             auth_ui.New_Product("Phase2_Refactor/inventory.json")
 
-        with tab2:
+        if st.session_state["employee_dashboard_view"] == "updating price":
             auth_ui.Update("Phase2_Refactor/inventory.json")
 
-        with tab3:
+        if st.session_state["employee_dashboard_view"] == "inventory refill":
             auth_ui.Restock("Phase2_Refactor/inventory.json")
 
-        with tab4:
+        if st.session_state["employee_dashboard_view"] == "discard item":
             auth_ui.Delete("Phase2_Refactor/inventory.json")
 
-        with tab5:
+        if st.session_state["employee_dashboard_view"] == "ai for owner":
             auth_ui.AI_Owner("Phase2_Refactor/AI.json")
 
 #Employee
 elif st.session_state["role"] == "Employee":
     if st.session_state["page"] == "home":
-        st.markdown("Welcome! This is the Employee dashboard")
-        if st.button("Go to Dashboard", key="dashboard_view_btn", type="primary", use_container_width=True):
-            st.session_state["page"] = "dashboard"
-            st.rerun()
+        st.markdown("Welcome! This is the Employee dashboard!")
+
     elif st.session_state["page"] == "dashboard":
-        st.markdown("Dashboard")
 
-        if st.button("Log out", type="primary", use_container_width=True):
-            st.session_state["logged_in"] = False
-            st.session_state["user"] = None
-            st.session_state["role"] = None
-            st.session_state["page"] = "login"
-            import time
-            time.sleep(1)
-            st.rerun()
-
-        # Define tabs for the employee dashboard
-        tab1, tab2, tab3, tab4 = st.tabs(["View Inventory", "Low Stock Items", "Daily Sales", "AI Helper"])
-
-        # First Tab: View Inventory
-        with tab1:
+        if st.session_state["employee_dashboard_view"] == "catalog":
             auth_ui.Cat()
 
-        # Second Tab: Low Stock Items
-        with tab2:
+        if st.session_state["employee_dashboard_view"] == "inventory":
             auth_ui.Inv()
 
-        # Third Tab: Daily Sales
-        with tab3:
+        if st.session_state["employee_dashboard_view"] == "rev":
             auth_ui.Revenue("Phase2_Refactor/inventory.json")
 
-
-        # Fourth Tab: AI Chatbot
-        with tab4:
+        if st.session_state["employee_dashboard_view"] == "ai for employee":
             auth_ui.AI_employee("Phase2_Refactor/AI.json")
+
+
+with st.sidebar:
+    st.subheader("**Account Info**")
+    if st.session_state["logged_in"] == True:
+        user = st.session_state["user"]
+        st.markdown(f"User Email: {user['email']}")
+        st.markdown(f"Logged In as: {user['role']}")
+
+        #Employee
+        if user["role"] == "Employee":
+            if st.button("Current Catalog", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "catalog"
+                st.rerun()
+        
+        if user["role"] == "Employee":
+            if st.button("Inventory", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "inventory"
+                st.rerun()
+
+        if user["role"] == "Employee":
+            if st.button("Daily Sales", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "rev"
+                st.rerun()
+
+        if user["role"] == "Employee":
+            if st.button("AI Chatbot", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "ai for employee"
+                st.rerun()
+
+        #Owner
+        if user["role"] == "Owner":
+            if st.button("Add New Product", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "adding product"
+                st.rerun()
+
+        if user["role"] == "Owner":
+            if st.button("Update Prices", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "updating price"
+                st.rerun()
+
+        if user["role"] == "Owner":
+            if st.button("Restock Inventory", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "inventory refill"
+                st.rerun()
+
+        if user["role"] == "Owner":
+            if st.button("Delete Discontinued Item(s)", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "discard item"
+                st.rerun()
+
+        if user["role"] == "Owner":
+            if st.button("AI Chatbot", type="secondary", use_container_width=True):
+                st.session_state["page"] = "dashboard"
+                st.session_state["employee_dashboard_view"] = "ai for owner"
+                st.rerun()
+
+        #Logout for both users
+        if st.button("Log out", type="primary", use_container_width=True):
+            log_out()
